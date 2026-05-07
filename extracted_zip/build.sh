b6f749d96894ea1cfb3dd53fec744f7f7de84bc8
Sai Suriyha JK <vexedpainter473@gmail.com>
2026-05-07 11:35:10 +0530

    chore: clean up extraction artifacts

--- extracted_zip/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Build script for Render deployment
-set -o errexit
-
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Generate seed data if not present
-python generate_data.py
